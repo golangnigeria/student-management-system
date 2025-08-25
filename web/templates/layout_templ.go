ecb8e0659b7ad319852e10e0e6c4e5a19784dc19
Dimkpa Prince <stackninja.pro>
2025-08-25 03:34:03 +0100

    Refactor header and layout templates for improved structure and styling; remove unnecessary Docker ignore file

--- web/templates/layout_templ.go
+++ web/templates/layout_templ.go
@@ -59,7 +59,7 @@ func Layout(td *models.TemplateData) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "</div></main><script src=\"https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js\"></script></body></html>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "</div></main></body></html>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
